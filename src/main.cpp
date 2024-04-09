479fd666ab188a6ce0b72ad733a61b29897eb168
Lázár Marcell <marcell.lazar2003@gmail.com>
2024-04-09 18:46:08 +0200

    Macros in separate function, clearer code

--- src/main.cpp
+++ src/main.cpp
@@ -3,31 +3,36 @@
 #include <HID-Project.h>
 #include <macros.h>
 
+
+/* =================== SETTING UP KEYMAPS =================== */
 const byte ROWS = 4;
 const byte COLS = 5;
+
 char keys[ROWS][COLS] = {
   {'0', '1', '2', '3', '4'},
   {'5', '6', '7', '8', '9'},
   {'A', 'B', 'C', 'D', 'E'},
   {'F', 'G', 'H', 'I', 'J'}
 };
+
+// For debugging with common 4x4 button matrix
+// char keys[ROWS][COLS] = {
+//   {'0', '1', '2', '3'},
+//   {'4', '5', '6', '7'},
+//   {'8', '9', 'A', 'B'},
+//   {'C', 'D', 'E', 'F'}
+// };
+
 byte rowPins[ROWS] = {5, 4, 3, 2};
-byte colPins[COLS] = {10, 9, 8, 7, 6};
+byte colPins[COLS] = {9, 8, 7, 6};
 
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
+
+/* ============== COUNTING LOOPS FOR DEBUGGING ============== */
 unsigned long loopCount;
 unsigned long startTime;
-String msg;
-
-void setup() {
-  Serial.begin(115200);
-  loopCount = 0;
-  startTime = millis();
-  msg = "";
-}
-
-void loop() {
+void averageLoopsPerSecond() {
   loopCount++;
   if ((millis() - startTime) > 5000) {
     Serial.print("Average loops per second = ");
@@ -35,25 +40,96 @@ void loop() {
     startTime = millis();
     loopCount = 0;
   }
+}
 
+
+/* ================= PAIRING MACROS TO KEYS ================= */
+void handleKey(char key, KeyState state) {
+  switch (key) {
+  case '0':
+    if (state == PRESSED) {
+    }
+    else if (state == HOLD) {
+    }
+  break;
+  case '1':
+    if (state == PRESSED) {
+    }
+    else if (state == HOLD) {
+    }
+  break;
+  case '2':
+    if (state == PRESSED) {
+    }
+    else if (state == HOLD) {
+    }
+  break;
+  case '3':
+    if (state == PRESSED) {
+    }
+    else if (state == HOLD) {
+    }
+  break;
+  case '4':
+    if (state == PRESSED) {
+    }
+    else if (state == HOLD) {
+    }
+  break;
+  case '5':
+  break;
+  case '6':
+  break;
+  case '7':
+  break;
+  case '8':
+  break;
+  case '9':
+  break;
+  case 'A':
+  break;
+  case 'B':
+  break;
+  case 'C':
+  break;
+  case 'D':
+  break;
+  case 'E':
+  break;
+  case 'F':
+  break;
+  case 'G':
+  break;
+  case 'H':
+  break;
+  case 'I':
+  break;
+  case 'J':
+  break;
+  default:
+  break;
+  }
+}
+
+
+/* ========================= SETUP ========================== */
+void setup() {
+  Serial.begin(115200);
+  loopCount = 0;
+  startTime = millis();
+}
+
+
+/* ========================== LOOP ========================== */
+void loop() {
+  averageLoopsPerSecond();
   //The keypad.key[] array can have 10 active keys at max
   if (keypad.getKeys()) {
     for (int i = 0; i < LIST_MAX; i++) {
       if (keypad.key[i].stateChanged) {
-        switch (keypad.key[i].kstate) {
-          case PRESSED:
-            msg = " PRESSED";
-            break;
-          case HOLD:
-            msg = " HELD";
-            break;
-          case RELEASED:
-            msg = " RELEASED";
-            break;
-          case IDLE:
-            msg = " IDLE";
-        }
-        Serial.print("Key " + keypad.key[i].kchar + msg);
+        char currentKey = keypad.key[i].kchar;
+        KeyState currentState = keypad.key[i].kstate;
+        handleKey(currentKey, currentState);
       }
     }
   }
